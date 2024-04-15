3bb4454db41e03ffefb184b7400e4fd78e9e1f49
ishan <isan91798@gmail.com>
2024-04-15 09:15:32 +0545

    week 1 task 2

---
+++ week 1/modify.java
@@ -0,0 +1,10 @@
+public class modify {
+    /*Extend/Modify the above program to print two additional lines:
+
+This is my first program
+
+I am on module ST4003CEM */
+    public static void main(String[] args) {
+        System.out.print("This is my first program\nI am on module ST4003CEM");
+    }
+}
