ce4bffbfcc339a2803f7349586ccb8663dde4e63
ishan <isan91798@gmail.com>
2024-04-15 09:58:31 +0545

    week 1 task 4

---
+++ week 1/task4.java
@@ -0,0 +1,18 @@
+public class task4 {
+    /*WAP that prints the following pattern:
+
+*
+
+**
+
+***
+
+**** */
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("**** */");
+
+    }
+}
